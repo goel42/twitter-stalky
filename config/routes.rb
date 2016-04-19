67e9fe03f3694a2c269132858d71bfd5a9944580
Anujraaj Goel <me.anujraaj@gmail.com>
2016-04-20 01:25:13 +0530

    Routes Configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :stalkees
+  root 'stalkees#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
