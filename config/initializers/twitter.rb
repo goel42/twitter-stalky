b69c60ecd06109eaacbf6b0b321af2310e2415ff
Anujraaj Goel <me.anujraaj@gmail.com>
2016-04-20 23:35:39 +0530

    UI added

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,3 +1,11 @@
+# proxy = {
+#   host: '172.31.1.4',
+#   port: 8080,
+#   username: 'iim2014002',
+#   password: 'Anuj9811410760',
+# }
+
+
 $client = Twitter::REST::Client.new do |config|
   # config.consumer_key        = "consumer_key"
   # config.consumer_secret     = "consumer_secret"
@@ -8,4 +16,5 @@ $client = Twitter::REST::Client.new do |config|
   config.consumer_secret     = "zrg5CG0G9SO1fQm8ISoOiR0BbZzpmCkDw8r6w4bkyhH58rkV3t"
   config.access_token        = "3785264952-eZjCJfJvqoQFJHrNw4rr0jJTuRaaAxVSLiZkgLq"
   config.access_token_secret = "dlsIzew6Fg0bPnG54aLzsNNcNY3rvkvcozwnX1nDuI9Vo"
+  # config.proxy 				 = proxy
 end
