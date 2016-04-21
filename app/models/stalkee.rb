627fa76289b847194e3145176b09cb271d69dc04
Anujraaj Goel <me.anujraaj@gmail.com>
2016-04-22 03:35:58 +0530

    Minor Bug Fix

--- app/models/stalkee.rb
+++ app/models/stalkee.rb
@@ -1,4 +1,4 @@
 class Stalkee < ActiveRecord::Base
-	validates :twitter_handle, presence: true, length: {maximum: 15,minimum:5},	uniqueness: { case_sensitive: false , message: "already exists in the Previously Stalked Table"}, format: { with: /^\w+$/, multiline: true, message: "only allows alphabets, numbers or _ allowed"}
+	validates :twitter_handle, presence: true, length: {maximum: 15,minimum:5},	uniqueness: { case_sensitive: false , message: "already exists in the Twiter Handles Table"}, format: { with: /^\w+$/, multiline: true, message: "only alphabets, numbers and _ allowed"}
 
 end
